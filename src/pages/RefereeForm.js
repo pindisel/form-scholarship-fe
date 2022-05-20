47b2798ca12c48c421ce343ac55059f60d4ab456
pindisel <72156714+pindisel@users.noreply.github.com>
2022-05-21 03:00:01 +0700

    testing

--- src/pages/RefereeForm.js
+++ src/pages/RefereeForm.js
@@ -42,65 +42,70 @@ const grade = [
 
 const RefereeForm = () => {
   const { id_user, id_ref } = useParams();
-  const navigate = useNavigate();
-  // console.log(id_user, id_ref);
-  const [personal, setPersonal] = useState([]);
-  const [referee, setReferee] = useState([]);
-  const [study, setStudy] = useState([]);
-  const [refereeForm, setRefereeForm] = useState({
-    id_user: id_user,
-  });
+  // const navigate = useNavigate();
+  // // console.log(id_user, id_ref);
+  // const [personal, setPersonal] = useState([]);
+  // const [referee, setReferee] = useState([]);
+  // const [study, setStudy] = useState([]);
+  // const [refereeForm, setRefereeForm] = useState({
+  //   id_user: id_user,
+  // });
 
-  const handleChange = (e) => {
-    setRefereeForm({ ...refereeForm, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   setRefereeForm({ ...refereeForm, [e.target.name]: e.target.value });
+  // };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setPersonal(
-        await (
-          await PersonalService.getPersonalById(id_user)
-        ).data.data
-      );
-      setReferee(
-        await (
-          await RefereeService.getRefereeById(id_user)
-        ).data.data
-      );
-      setStudy(await (await StudyService.getStudyById(id_user)).data.data);
-      if (id_ref === 1) {
-        var response = await RefereeFormService.getRef1ById(id_user);
-      } else if (id_ref === 2) {
-        response = await RefereeFormService.getRef2ById(id_user);
-      }
-      if (response.data.data) {
-        navigate("/");
-        window.location.reload();
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setPersonal(
+  //       await (
+  //         await PersonalService.getPersonalById(id_user)
+  //       ).data.data
+  //     );
+  //     setReferee(
+  //       await (
+  //         await RefereeService.getRefereeById(id_user)
+  //       ).data.data
+  //     );
+  //     setStudy(await (await StudyService.getStudyById(id_user)).data.data);
+  //     if (id_ref === 1) {
+  //       var response = await RefereeFormService.getRef1ById(id_user);
+  //     } else if (id_ref === 2) {
+  //       response = await RefereeFormService.getRef2ById(id_user);
+  //     }
+  //     if (response.data.data) {
+  //       navigate("/");
+  //       window.location.reload();
+  //     }
+  //   };
 
-    fetchData();
-  }, [id_user, id_ref, navigate]);
+  //   fetchData();
+  // }, [id_user, id_ref, navigate]);
   // console.log(refereeForm);
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      if (id_ref === 1) {
-        await RefereeFormService.createRef1(refereeForm);
-      } else if (id_ref === 2) {
-        await RefereeFormService.createRef2(refereeForm);
-      }
-      alert("Data successfully submitted");
-      navigate("/");
-      window.location.reload();
-    } catch (error) {
-      // console.log(error);
-      alert("Error submitting data");
-    }
-  };
+  // const onSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     if (id_ref === 1) {
+  //       await RefereeFormService.createRef1(refereeForm);
+  //     } else if (id_ref === 2) {
+  //       await RefereeFormService.createRef2(refereeForm);
+  //     }
+  //     alert("Data successfully submitted");
+  //     navigate("/");
+  //     window.location.reload();
+  //   } catch (error) {
+  //     // console.log(error);
+  //     alert("Error submitting data");
+  //   }
+  // };
 
-  return <>asdadas</>;
+  return (
+    <>
+      {id_ref}
+      {id_user}
+    </>
+  );
 };
 
 export default RefereeForm;
